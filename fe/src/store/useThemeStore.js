ba3e9c21b90713cbc0ba77c3a9d8178d20805592
harsHdokyc <harsh@1stopkyc.com>
2025-07-01 12:56:05 +0530

    Update favicon to PNG format, replace SVG icon, and change default theme to 'dark'; remove unused images from public directory.

--- fe/src/store/useThemeStore.js
+++ fe/src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("TalkIn-theme") || "coffee",
+  theme: localStorage.getItem("TalkIn-theme") || "dark",
   setTheme: (theme) => {
     localStorage.setItem("TalkIn-theme", theme);
     set({ theme });
