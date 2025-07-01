c50b98542e2f423a8d29b0e0e3c170384bf29d27
harsHdokyc <harsh@1stopkyc.com>
2025-07-01 08:48:14 +0530

    Add 'talkin' package and update server configuration for production

--- be/src/server.js
+++ be/src/server.js
@@ -6,9 +6,11 @@ import chatRoutes from "./routes/chat.route.js";
 import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";    
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
@@ -19,6 +21,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../fe/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../fe/dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
